b79abe736535b4149e4b11b5bee640cd2a924daf
DrDead0 <112349103+DrDead0@users.noreply.github.com>
2025-11-15 19:23:05 +0530

    Added multer support

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,3 +3,8 @@ import registerUser from "../controllers/registerUser.controller.js";
 const userRouter = Router();
 
 userRouter.post("/register", registerUser);
+userRouter.post("/login", loginUser);
+
+
+
+export {userRouter,userLogin} 
